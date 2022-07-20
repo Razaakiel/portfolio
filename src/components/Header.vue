b27aaac5c8aadfa7a570eeb0291f9714479b758e
Yoann POULAIN <yoann-du-95@hotmail.fr>
2022-07-20 14:21:26 +0200

    mise en place du header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,67 @@
 <template>
-$END$
+<div class="header">
+
+  <div class="header__profil">
+    <h1 class="header__profil-name">Yoann POULAIN</h1>
+    <img src="../assets/pp.jpeg" alt="photo de profil" class="header__profil-photo">
+    <h2 class="header__profil-metier">Développeur - Frontend</h2>
+  </div>
+
+  <div class="header__nav">
+    <div class="header__nav-accueil">
+      <router-link to="/accueil">Accueil</router-link>
+    </div>
+    <div class="header__nav-profil">
+        <router-link to="/profil">Mon Profil</router-link>
+    </div>
+    <div class="herder__nav-projets">
+      <router-link to="/projet">Mes projets</router-link>
+    </div>
+    <div class="header__nav-linkedin">
+      <a href="https://www.linkedin.com/in/yoann-poulain-1a6289153/">Linkedin</a>
+    </div>
+    <div class="header__nav-github">
+      <a href="https://github.com/Razaakiel">Github</a>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-name: "Header.vue"
+  name: "Header"
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.header{
+  background-color: #213f77;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  padding:20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.header{
+
+  &__profil-photo{
+    margin-left: 55px;
+   }
+
+  &__nav{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-left: 90px;
+    height: 200px;
+  }
+}
+
 
 </style>
---
+++ src/views/Accueil.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="accueil">
+    <h2 class="accueil__title">Accueil</h2>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  // eslint-disable-next-line
+  name: "Accueil.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
